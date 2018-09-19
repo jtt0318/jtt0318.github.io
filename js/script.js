dd51337dae805ad776f15a0bce54478aaa676cf2
jtt0318 <37620569+jtt0318@users.noreply.github.com>
2018-09-19 13:17:07 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-var music=document.getElementById("bgMusic");
- music.play();
- var audio=document.getElementsByClassName("zhong");
-    audio.addEventListener("click",function(){
-        music.pause();
-        audio.style.background="url(images/play.png) no-repeat";
-    });
-
 
