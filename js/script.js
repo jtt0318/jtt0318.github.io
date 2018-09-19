53dbd1bdb5955368c95c8cf7d324fb5b8c87a380
jtt0318 <37620569+jtt0318@users.noreply.github.com>
2018-09-19 15:33:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', bindEvent)
+var button= document.querySelector('.button');;
+function bindEvent() {
+    button.addEventListener('click', function() {
+        var isPlaying=button.classList.contains("play")==true;
+        if(isPlaying){
+            document.getElementById("bgMusic").pause();
+            button.classList.remove('play');
+            button.classList.add('pause');
+        }
+        else{
+            document.getElementById("bgMusic").play();
+            button.classList.remove('pause');
+            button.classList.add('play');
+        }
 
+    })
+}
