7219cb1f10fdb7ba1e1cc0dad1426a0fb7da24b6
jtt0318 <37620569+jtt0318@users.noreply.github.com>
2018-09-28 16:09:57 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', bindEvent)
+
 var button= document.querySelector('.button');;
 function bindEvent() {
     button.addEventListener('click', function() {
@@ -15,4 +16,22 @@ function bindEvent() {
         }
 
     })
+    nextPage();
+    upPage();
 }
+var pageFirst=document.getElementsByClassName("page_first")[0];
+var pageTwo=document.getElementsByClassName("page_two")[0];
+function nextPage(){
+    var downPage=document.getElementById("right");
+    downPage.addEventListener("click",function(){
+        pageFirst.style.display='none';
+        pageTwo.style.display="block";
+    })
+}
+function upPage(){
+    var upPage=document.getElementsByClassName("pagetwo_left")[0];
+        upPage.addEventListener("click",function(){
+            pageTwo.style.display="none";
+            pageFirst.style.display='block';
+        })
+}   
