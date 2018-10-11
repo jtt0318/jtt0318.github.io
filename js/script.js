c916c201f993c7bbf52bc72e8a00de03c660fe4d
jtt0318 <37620569+jtt0318@users.noreply.github.com>
2018-10-11 19:05:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,11 +21,17 @@ function bindEvent() {
 }
 var pageFirst=document.getElementsByClassName("page_first")[0];
 var pageTwo=document.getElementsByClassName("page_two")[0];
+var pageThree=document.getElementsByClassName("page_three")[0];
 function nextPage(){
     var downPage=document.getElementById("right");
     downPage.addEventListener("click",function(){
         pageFirst.style.display='none';
         pageTwo.style.display="block";
+    })
+    var nextPage=document.getElementsByClassName("pagetwo_right")[0];
+    nextPage.addEventListener("click",function(){
+        pageTwo.style.display="none";
+        pageThree.style.display="block";
     })
 }
 function upPage(){
@@ -33,5 +39,10 @@ function upPage(){
         upPage.addEventListener("click",function(){
             pageTwo.style.display="none";
             pageFirst.style.display='block';
+        })
+    var upPage3=document.getElementsByClassName("pagethree_left")[0];
+        upPage3.addEventListener("click",function(){
+            pageThree.style.display="none";
+            pageTwo.style.display="block";
         })
 }   
